439ff3003a4499afca6a5814702ff1cd5e2e5ffe
bayeka binet <bayekabinet@gmail.com>
2025-11-17 15:44:50 +0100

    changes

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -3,10 +3,7 @@ import { Link } from "react-router-dom";
 
 import { Button } from "./Button";
 import {
-  AirVent,
-  BookOpen,
-  FileAxis3D,
-  Hamburger,
+  
   Menu,
   X,
 } from "lucide-react";
